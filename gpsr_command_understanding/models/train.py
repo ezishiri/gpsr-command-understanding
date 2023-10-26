70a466dd7e70647fed0955f41cb0fd2eccaff913
Edwin Zishiri <edwin.zishiri@yale.edu>
2023-10-25 23:28:34 -0400

    super broken, but workable training skeleton code

--- gpsr_command_understanding/models/train.py
+++ gpsr_command_understanding/models/train.py
@@ -1,46 +1,152 @@
-# finetune T5 using huggingface on commands dataset
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-import torch
+import transformers
+from datasets import load_dataset, load_metric
 
-tokenizer = T5Tokenizer.from_pretrained("t5-base")
-model = T5ForConditionalGeneration.from_pretrained("t5-base")
-
-
-#TODO: Setup the trainer to use the T5 fine tuner and code loss function myself (simple cross entropy loss)
-
-# the following 2 hyperparameters are task-specific
-max_source_length = 512
-max_target_length = 512 
+# load our robot commands dataset 
+dataset = load_dataset('ezishiri/robot_commands')
 
 
-# encode the inputs
-task_prefix = "translate utterance to logical form: "
-input_sequences = [] # input sequences from hf dataset we are loading 
-output_sequences = []# output sequences from dataset
 
-encoding = tokenizer(
-    [task_prefix + sequence for sequence in input_sequences],
-    padding="longest",
-    max_length=max_source_length,
-    truncation=True,
-    return_tensors="pt",
+
+import nltk
+nltk.download('punkt')
+import string
+from transformers import AutoTokenizer
+
+model_checkpoint = "t5-base"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+
+
+# BELOW IS CODE TAKEN FROM https://medium.com/nlplanet/a-full-guide-to-finetuning-t5-for-text2text-and-building-a-demo-with-streamlit-c72009631887
+
+# NEED TO FIX AND CHANGE FOR MY USECASE (less data cleaning, only need to remove \n)
+
+
+prefix = "summarize: " # need prefix for good performance with t5 
+max_input_length = 512
+max_target_length = 64
+
+def clean_text(text):
+  sentences = nltk.sent_tokenize(text.strip())
+  sentences_cleaned = [s for sent in sentences for s in sent.split("\n")]
+  sentences_cleaned_no_titles = [sent for sent in sentences_cleaned
+                                 if len(sent) > 0 and
+                                 sent[-1] in string.punctuation]
+  text_cleaned = "\n".join(sentences_cleaned_no_titles)
+  return text_cleaned
+
+def preprocess_data(examples):
+  texts_cleaned = [clean_text(text) for text in examples["text"]]
+  inputs = [prefix + text for text in texts_cleaned]
+  model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
+
+  # Setup the tokenizer for targets
+  with tokenizer.as_target_tokenizer():
+    labels = tokenizer(examples["title"], max_length=max_target_length, 
+                       truncation=True)
+
+  model_inputs["labels"] = labels["input_ids"]
+  return model_inputs
+
+
+
+
+tokenized_datasets = medium_datasets_cleaned.map(preprocess_function,
+                                                 batched=True)
+
+
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq,
+  Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+
+batch_size = 8
+model_name = "t5-base-medium-title-generation"
+model_dir = f"drive/MyDrive/Models/{model_name}"
+
+args = Seq2SeqTrainingArguments(
+    model_dir,
+    evaluation_strategy="steps",
+    eval_steps=100,
+    logging_strategy="steps",
+    logging_steps=100,
+    save_strategy="steps",
+    save_steps=200,
+    learning_rate=4e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=1,
+    predict_with_generate=True,
+    fp16=True,
+    load_best_model_at_end=True,
+    metric_for_best_model="rouge1",
+    report_to="tensorboard"
 )
 
-input_ids, attention_mask = encoding.input_ids, encoding.attention_mask
 
-# encode the targets
-target_encoding = tokenizer(
-    [], # add list of output sequences 
-    padding="longest",
-    max_length=max_target_length,
-    truncation=True,
-    return_tensors="pt",
+
+data_collator = DataCollatorForSeq2Seq(tokenizer)
+
+
+metric = load_metric("rouge")
+
+
+import numpy as np
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+    # Rouge expects a newline after each sentence
+    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip()))
+                      for pred in decoded_preds]
+    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) 
+                      for label in decoded_labels]
+    
+    # Compute ROUGE scores
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels,
+                            use_stemmer=True)
+
+    # Extract ROUGE f1 scores
+    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+    
+    # Add mean generated length to metrics
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id)
+                      for pred in predictions]
+    result["gen_len"] = np.mean(prediction_lens)
+    
+    return {k: round(v, 4) for k, v in result.items()}
+
+
+
+
+# Function that returns an untrained model to be trained
+def model_init():
+    return AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
+
+trainer = Seq2SeqTrainer(
+    model_init=model_init,
+    args=args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
 )
-labels = target_encoding.input_ids
 
-# replace padding token id's of the labels by -100 so it's ignored by the loss
-labels[labels == tokenizer.pad_token_id] = -100
 
-# forward pass
-loss = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels).loss
-loss.item()
+trainer.train()
+
+
+
+
+
+
+
+
+
