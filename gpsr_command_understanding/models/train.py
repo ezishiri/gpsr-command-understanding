1d63a0815e92975468faab0323bf18b977e9f331
Edwin Zishiri <edwin.zishiri@yale.edu>
2023-10-31 00:20:30 -0400

    set up hf seq2seq trainer for training

--- gpsr_command_understanding/models/train.py
+++ gpsr_command_understanding/models/train.py
@@ -1,48 +1,53 @@
 import transformers
-from datasets import load_dataset, load_metric
+from datasets import load_dataset
+import evaluate 
+from transformers import Seq2SeqTrainer
+# import wandb
 
 # load our robot commands dataset 
 dataset = load_dataset('ezishiri/robot_commands')
 
+print(dataset)
 
 
-
-import nltk
-nltk.download('punkt')
+# import nltk
+# nltk.download('punkt')
 import string
 from transformers import AutoTokenizer
 
 model_checkpoint = "t5-base"
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, model_max_length=512)
+
+
 
 
 
 # BELOW IS CODE TAKEN FROM https://medium.com/nlplanet/a-full-guide-to-finetuning-t5-for-text2text-and-building-a-demo-with-streamlit-c72009631887
 
-# NEED TO FIX AND CHANGE FOR MY USECASE (less data cleaning, only need to remove \n)
+# NEED TO FIX AND CHANGE FOR MY USECASE (less data cleaning, only need to remove \n at some point, but for now should be fine
 
 
-prefix = "summarize: " # need prefix for good performance with t5 
+prefix = "translate command utterance to logical form: " # need prefix for good performance with t5 
 max_input_length = 512
-max_target_length = 64
+max_target_length = 512
 
-def clean_text(text):
-  sentences = nltk.sent_tokenize(text.strip())
-  sentences_cleaned = [s for sent in sentences for s in sent.split("\n")]
-  sentences_cleaned_no_titles = [sent for sent in sentences_cleaned
-                                 if len(sent) > 0 and
-                                 sent[-1] in string.punctuation]
-  text_cleaned = "\n".join(sentences_cleaned_no_titles)
-  return text_cleaned
+# def clean_text(text):
+#   sentences = nltk.sent_tokenize(text.strip())
+#   sentences_cleaned = [s for sent in sentences for s in sent.split("\n")]
+#   sentences_cleaned_no_titles = [sent for sent in sentences_cleaned
+#                                  if len(sent) > 0 and
+#                                  sent[-1] in string.punctuation]
+#   text_cleaned = "\n".join(sentences_cleaned_no_titles)
+#   return text_cleaned
 
 def preprocess_data(examples):
-  texts_cleaned = [clean_text(text) for text in examples["text"]]
+  texts_cleaned = [text for text in examples["utterance"]]
   inputs = [prefix + text for text in texts_cleaned]
   model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
 
   # Setup the tokenizer for targets
   with tokenizer.as_target_tokenizer():
-    labels = tokenizer(examples["title"], max_length=max_target_length, 
+    labels = tokenizer(examples["logical_form"], max_length=max_target_length, 
                        truncation=True)
 
   model_inputs["labels"] = labels["input_ids"]
@@ -51,17 +56,16 @@ def preprocess_data(examples):
 
 
 
-tokenized_datasets = medium_datasets_cleaned.map(preprocess_function,
+tokenized_datasets = dataset.map(preprocess_data,
                                                  batched=True)
 
 
-from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq,
-  Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
 
 
 batch_size = 8
-model_name = "t5-base-medium-title-generation"
-model_dir = f"drive/MyDrive/Models/{model_name}"
+model_name = "t5-base-robot-command-understanding"
+model_dir = f"./{model_name}" # save the model in the models directory we are currently working in 
 
 args = Seq2SeqTrainingArguments(
     model_dir,
@@ -76,12 +80,13 @@ args = Seq2SeqTrainingArguments(
     per_device_eval_batch_size=batch_size,
     weight_decay=0.01,
     save_total_limit=3,
-    num_train_epochs=1,
+    num_train_epochs=150,
     predict_with_generate=True,
-    fp16=True,
+    # fp16=True, # no fp16 for now while testing on my machine 
     load_best_model_at_end=True,
-    metric_for_best_model="rouge1",
-    report_to="tensorboard"
+    metric_for_best_model="exact_match",
+    push_to_hub=True,
+    # report_to="wandb"  no reporting for now, need to set up personal wandb account 
 )
 
 
@@ -89,24 +94,18 @@ args = Seq2SeqTrainingArguments(
 data_collator = DataCollatorForSeq2Seq(tokenizer)
 
 
-metric = load_metric("rouge")
+metric = evaluate.load("exact_match")
 
 
 import numpy as np
 
-def compute_metrics(eval_pred):
+def compute_metrics(eval_pred): # probably not going to work for exact match calculations 
     predictions, labels = eval_pred
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
     
     # Replace -100 in the labels as we can't decode them.
     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-    
-    # Rouge expects a newline after each sentence
-    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip()))
-                      for pred in decoded_preds]
-    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) 
-                      for label in decoded_labels]
     
     # Compute ROUGE scores
     result = metric.compute(predictions=decoded_preds, references=decoded_labels,
@@ -140,7 +139,7 @@ trainer = Seq2SeqTrainer(
 )
 
 
-trainer.train()
+# trainer.train()
 
 
 
